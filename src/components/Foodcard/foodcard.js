a18597f72c3cb5564cb41147d3022439a5f5157a
Shrushti110 <giramkarshrushti@gmail.com>
2024-09-15 21:13:31 +0530

    images added

--- src/components/Foodcard/foodcard.js
+++ src/components/Foodcard/foodcard.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './foodcard.css'
+import eatIcon from "./eat.png"
+import drinkIcon from "./drink.png"
 
-function foodcard(props) {
+function foodcard({imgUrl, title , description, isEatType}) {
+
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
+    <div className='foodcard'>
+      <img src={imgUrl}
+      className='foodcard-image'
+      alt='food'/>
+
+      <h1 className='foodcard-title'>{title}</h1>
+      <p>{description}</p>
+
+      <img src={isEatType? eatIcon : drinkIcon} 
+      className='foodcard-icon' 
+      alt='salad'/>
     </div>
   )
 }
--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -10,13 +10,30 @@ function home() {
       <h1>Seven Eleven Convenience Store</h1>
 
        <Foodcard 
+       imgUrl="https://img.freepik.com/free-photo/flat-lay-salad-with-chicken-sesame-seeds_23-2148700369.jpg?t=st=1726414744~exp=1726418344~hmac=ca08743012149d76ab
+       3dd0dac2f141f3a8ddf9dc9684bc5716a1079c765fbed9&w=900"
        title="Salad" 
-       description="Enjoy this fresh 'Salad' with crisp greens and a light vinaigrette."/>
+       description="Enjoy this fresh 'Salad' with crisp greens and a light vinaigrette."
+       isEatType={true}
+       />
 
        <Foodcard
        title="Energy drinks"
-       description="Boost your energy with our selection of energy drinks."/>
-       
+       description="Boost your energy with our selection of energy drinks."
+       isEatType={false}
+       />
+
+       <Foodcard
+       title={"Cookies"}
+       description={"Indulge in our delicious cookies, perfect for a sweet treat."}
+       isEatType={true}
+       />
+
+       <Foodcard
+       title={"Coffee"}
+       description={"Start your day off right with a hot cup of coffee."}
+       isEatType={false}
+       />
     </div>
   )
 }
